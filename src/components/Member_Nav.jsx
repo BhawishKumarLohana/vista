e3c49ffa0508bb359f9ccaa50104fdcb025d487c
Bhawish Kumar <bhawishkumar000@gmail.com>
2025-05-05 17:53:23 +0800

    ProjectSetup#1

---
+++ src/components/Member_Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Member_Nav() {
+  return (
+    <div>Member_Nav</div>
+  )
+}
+
+export default Member_Nav
---
+++ src/components/Visitor_Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Visitor_Nav() {
+  return (
+    <div>Visitor_Nav</div>
+  )
+}
+
+export default Visitor_Nav
