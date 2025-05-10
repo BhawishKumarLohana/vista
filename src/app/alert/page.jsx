2d0be7a749f20a1e38b17aac97e0451a30c4a619
Bhawish Kumar <bhawishkumar000@gmail.com>
2025-05-11 01:31:23 +0800

    SelectionofCoins

--- src/app/alert/page.jsx
+++ src/app/alert/page.jsx
@@ -3,6 +3,7 @@
 import CoinSelect from "@/components/CoinSelect"; // Adjust the path if necessary
 
 export default function AlertPage() {
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-black via-gray-900 to-purple-900">
       <h2 className="text-3xl font-bold text-white mb-6">Create Alert</h2>
