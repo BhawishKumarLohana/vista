2219903af3f344e53ac955b2bd72bb71473cf403
Bhawish Kumar <bhawishkumar000@gmail.com>
2025-05-11 00:18:35 +0800

    fix_alert

--- src/app/alert/page.jsx
+++ src/app/alert/page.jsx
@@ -1,5 +1,4 @@
-'use client';
-
+// PLEASE DO NOT MAKE ANY OF COMPONENTS GETTING DATA AS CLIENT
 // pages/alert.js
 import CoinSelect from "@/components/CoinSelect"; // Adjust the path if necessary
 
@@ -8,7 +7,6 @@ export default function AlertPage() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-black via-gray-900 to-purple-900">
       <h2 className="text-3xl font-bold text-white mb-6">Create Alert</h2>
       <CoinSelect />
-      {/* Other alert form fields can go here */}
     </div>
   );
 }
