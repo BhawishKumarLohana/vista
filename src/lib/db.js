f433a16b55eef6b4372c762335089805d2cfb7f0
Bhawish Kumar <23205172@life.hkbu.edu.hk>
2025-05-19 01:44:51 +0800

    showCoins_to_sql

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function ShowCoins (){
-  await new Promise((resolve)=>setTimeout(resolve,5000))
-  return prisma.coin.findMany();
+export async function ShowCoins() {
+  await new Promise((resolve) => setTimeout(resolve, 100));
+
+  const coins = await prisma.$queryRawUnsafe(`
+    SELECT * FROM Coin
+  `);
+
+  return coins;
 }
 
 export default prisma;
