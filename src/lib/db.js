108e77b13b275d3b5dbc6d65c4330fe00e5caba7
Bhawish Kumar <23205172@life.hkbu.edu.hk>
2025-06-01 01:59:36 +0800

    change#4

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (typeof global !== 'undefined') {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+    global.prisma.$connect(); // optional: eager connect
+  }
+  prisma = global.prisma;
+} else {
+  prisma = new PrismaClient();
+}
+
 
 export async function ShowCoins() {
   await new Promise((resolve) => setTimeout(resolve, 100));
